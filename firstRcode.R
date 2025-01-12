ce06730c52540fcef8a452aa7a9f949fa7b2093f
Matina Kpassira <kpassiramat@gmail.com>
2025-01-11 20:57:04 -0800

    Create firstRcode.R

---
+++ firstRcode.R
@@ -0,0 +1,44 @@
+library(datasets)
+
+#Load Data
+
+data(mtcars)
+
+#View first 5 rows
+
+head(mtcars, 5)
+# get info about the dataset
+?mtcars
+
+#Install ggplot
+install.packages("ggplot2")
+
+#load ggplot package
+library(ggplot2)
+
+#create a scatterplot of displacement (disp) and miles per gallon (mpg)
+
+ggplot(aes(x=disp,y=mpg,),data=mtcars)+geom_point()
+
+#Add a title
+
+ggplot(aes(x=disp,y=mpg,),data=mtcars)+geom_point()+ggtitle("displacement vs miles per gallon")
+
+#change axis name
+
+ggplot(aes(x=disp,y=mpg,),data=mtcars)+geom_point()+ggtitle("displacement vs miles per gallon") + labs(x = "Displacement", y = "Miles per Gallon")
+
+#make vs a factor
+mtcars$vs <- as.factor(mtcars$vs)
+
+#create boxplot of the distribution for v-shaped and straight Engine
+
+ggplot(aes(x=vs, y=mpg), data = mtcars) + geom_boxplot()
+
+# Add color
+ggplot(aes(x=vs, y=mpg, fill = vs), data = mtcars) + 
+  geom_boxplot(alpha=0.3) +
+  theme(legend.position="none")
+
+# histogram of weight
+ggplot(aes(x=wt),data=mtcars) + geom_histogram(binwidth=0.5)
